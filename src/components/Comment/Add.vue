8d9a23f93a48ec0522152f6fdecd875c6a4d09ff
junjiezeng <zengjunj21@sina.com>
2020-03-14 19:57:31 +0800

    评论demo

---
+++ src/components/Comment/Add.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="col-md-4">
+    <form class="form-horizontal">
+        <div class="form-group">
+        <label>用户名</label>
+        <input type="text" class="form-control" placeholder="用户名" v-model = "name">
+        </div>
+        <div class="form-group">
+        <label>评论内容</label>
+        <textarea class="form-control" rows="6" placeholder="评论内容" v-model = "title"></textarea>
+        </div>
+        <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="button" class="btn btn-default pull-right" @click="addItem">提交</button>
+        </div>
+        </div>
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        add:Function,
+        
+    },
+    data(){
+        return{
+            name:'',
+            title:''
+        }
+    },
+    methods:{
+        // 添加
+        addItem(){
+            //取出表单内容
+            const {name, title,add } = this;
+            if(!name || !title){
+                alert("内容不能为空");
+                return 
+            }
+            // 组合对象
+            const obj = {
+                name,
+                title
+            }
+            // 调用添加方法
+            add(obj);
+            // 清除表单数据
+            this.name = '';
+            this.title = '';
+
+
+
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Comment/CommentApp.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对React的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <!-- Add -->
+      <Add :add = "add"/>
+      <!-- List -->
+      <List :comments = "comments" :deleted = "deleted"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './Add'
+import List from './List'
+export default {
+    data(){
+      return{
+        comments:[
+          {name:'Tom',title:'react 还不错'},
+          {name:'jack',title:'react ....'}
+        ]
+      }
+    },
+    methods:{
+      add(obj){
+        this.comments.unshift(obj)
+      },
+      deleted(index){
+        this.comments.splice(index,1)
+
+      }
+    },
+    components:{
+        Add,
+        List
+    }
+    
+}
+
+
+/*
+  -- 数组在哪个组件，更新数据行为就因该在哪里蒂尼
+  -- 组件化，使用组件必须通过components进行映射
+  -- v-model 表单数据自动收集（双向数据绑定）
+  -- 组件通信：一个组件接收父组件传递的数据声明接收属性props,接收过来的属性成为组件对象的属性
+  -- 
+
+*/
+</script>
+
+<style>
+
+</style>
